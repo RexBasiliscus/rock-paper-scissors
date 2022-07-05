3d4a843323d1c0282c6b74db6e46289b1bcf4596
RexBasiliscus <alexander-219@outlook.com>
2022-07-05 13:15:18 +0200

    created the function to compare (randomized) computerSelection with inputed playerSelection and return the result of the game

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const choices = ["Rock", "Paper", "Scissors"];
-
-function computerPlay (arr) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-
-  const item = arr[randomIndex];
-  
-  return item; 
+function computerPlay () {
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)]
 }
 
-const computerResult = computerPlay(choices);
-console.log(computerResult);
+
+function playRound (playerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  let computerSelection = computerPlay();
+
+  if (playerSelection === computerSelection) {
+    return "It\'s a Tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") || 
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `Congratz! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}!`
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") || 
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    return `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}!`
+  }
+}
+
+
