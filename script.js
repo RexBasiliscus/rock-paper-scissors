537b751576a4a538e0204f538e83607344c00b43
RexBasiliscus <alexander-219@outlook.com>
2022-07-07 13:29:59 +0200

    added the loop to play 5 rounds, the winner declaration and fixed the game function to work as intended

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
 let computerScore = 0;
 let playerScore = 0;
-let round;
+let round = 0;
 let choices = ["rock", "paper", "scissors"];
 
 
-// Displays a prompt box, where you input your choice
-let playerChoice = prompt('Choose either Rock, Paper, or Scissors');
-let playerSelection = playerChoice.toLowerCase();
-if (choices.includes(playerSelection)) {
-  playRound(playerSelection);
-} else {
-  alert('Please enter one of the options');
+// Starts the game, then displays a prompt box, where you input your choice; if the choice is not valid, it returns to start
+function game() {
+  let playerChoice = prompt('Choose either Rock, Paper, or Scissors');
+  let playerSelection = playerChoice.toLowerCase();
+  if (choices.includes(playerSelection)) {
+    playRound(playerSelection);
+  } else {
+    alert('Please enter one of the options');
+    game();
+  }
+}
+      
+function playRound (playerSelection) {
+  
+  // Outputs a randomized choice for the computer
+  function computerPlay () {
+    return choices[Math.floor(Math.random() * choices.length)]
+  }
+  let computerSelection = computerPlay();
+
+  // Compares the player vs. computer selections and outputs a specific text and increments score
+  if (playerSelection === computerSelection) {
+    alert("It\'s a Tie!");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") || 
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    alert(`Congratz! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}!`);
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") || 
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    computerScore++;
+    alert(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}!`);
+  }
+  round++;
+  // outputs the score
+  console.log(`Player: ${playerScore}`);
+  console.log(`Computer: ${computerScore}`);
+  console.log(`Round #${round}`);
 }
 
-      
-    function playRound () {
-      
-      // Outputs a randomized choice for the computer
-      function computerPlay () {
-        return choices[Math.floor(Math.random() * choices.length)]
-      }
-      let computerSelection = computerPlay();
+// looping through the game to play 5 rounds
+for (let round = 1; round < 6; round++) {
+  game();
+}
 
-      // Compares the player vs. computer selections and outputs a specific text and increments score
-      if (playerSelection === computerSelection) {
-        alert("It\'s a Tie!");
-      } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") || 
-        (playerSelection === "scissors" && computerSelection === "paper")
-      ) {
-        playerScore++;
-        alert(`Congratz! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}!`);
-      } else if (
-        (computerSelection === "rock" && playerSelection === "scissors") ||
-        (computerSelection === "paper" && playerSelection === "rock") || 
-        (computerSelection === "scissors" && playerSelection === "paper")
-      ) {
-        computerScore++;
-        alert(`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}!`);
-      }
-    }
+// after 5 rounds, declare a winner
+if (round === 5 && playerScore > computerScore) {
+  alert("You\'re Victorious!");
+} else if (round === 5 && computerScore > playerScore) {
+  alert("Oh no! The computer has won!")
+} else if (round === 5 && computerScore === playerScore) {
+  alert("No one wins, it\'s a Tie!")
+}
 
 
 
