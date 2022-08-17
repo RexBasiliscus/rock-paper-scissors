5b23c8154194b7852c53c894cc32a1fb367da33d
RexBasiliscus <alexander-219@outlook.com>
2022-08-17 17:09:01 +0200

    Fixed the start and restart btns functionalities, improved the end game logic.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//================== INITIAL CACHING =====================//
+
 let computerScore = 0;
 let playerScore = 0;
 let playerSelection;
 let computerSelection;
+let startButton = true;
 
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
@@ -9,10 +12,27 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 let gameMessage_div = document.getElementById("game-message");
-const wrapper_div = document.querySelector("wrapper");
-const choices_div = document.querySelector("choices");
-const instructionBox_div = document.querySelector("instruction-box");
+const choices_div = document.getElementById("choices");
 const startButton_div = document.getElementById("start");
+const gameBox_div = document.getElementById("game-box");
+
+//================== START BUTTON =====================//
+
+startButton_div.addEventListener('click', toggleStartBtn); 
+
+function toggleStartBtn() {  
+  if(startButton) {
+    gameBox_div.style.visibility = 'visible';
+    startButton_div.style.display = 'none';
+    startButton = false;
+  } else {
+    gameBox_div.style.visibility = 'hidden';
+    startButton_div.style.display = '';
+    startButton = true;
+  }
+}
+
+//================== RPS BTNS =====================//
 
 rock_div.addEventListener('click', () => userPlay("rock"));
 paper_div.addEventListener('click', () => userPlay("paper"));
@@ -23,10 +43,14 @@ function userPlay(choice) {
     game();
 }
 
+//================== COMPUTER CHOICE =====================//
+
 function computerPlay() {
   const choices = ["rock", "paper", "scissors"];
-  return choices[Math.floor(Math.random() * choices.length)]
+  return choices[Math.floor(Math.random() * choices.length)];
 }
+
+//================== PLAY A ROUND & SCORE =====================//
 
 function playRound(playerSelection) {
 
@@ -71,24 +95,45 @@ function tie() {
   setTimeout(() => document.getElementById(playerSelection).classList.remove('gray-bckgr'), 300);
 }
 
+//================== GAME FUNCTIONALITY =====================//
+
 function game() {
   if (!(playerScore > 4 || computerScore > 4)) {
     playRound(playerSelection);
   } else {
+    choices_div.style.display = 'none';
     endGame();
   }
 };
 
-// function to end the game, declare winner, append the restart btn
 function endGame() {
   if (playerScore > 4) {
     gameMessage_div.textContent = `Congratz! You have Won the game!`;
   } else if (computerScore > 4) {
     gameMessage_div.textContent = `You have lost the game :(`
   }
+
   const restartMsg = document.createElement("p");
   restartMsg.textContent = "Do you want to restart?"
   gameMessage_div.appendChild(restartMsg);
+
+  const endGameBtn = document.createElement("button");
+  endGameBtn.textContent = "Restart";
+  gameMessage_div.appendChild(endGameBtn);
+  
+  endGameBtn.addEventListener('click', () => {
+    computerScore = 0;
+    computerScore_span.textContent = 0;
+    playerScore = 0;
+    userScore_span.textContent = 0;
+    gameMessage_div.removeChild(restartMsg);
+    gameMessage_div.removeChild(endGameBtn);
+    gameMessage_div.textContent = '';
+    choices_div.style.display = '';
+    toggleStartBtn();
+  })
 }
+
+
 
 
